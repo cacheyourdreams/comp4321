55aafbd38af14c3ecb14f33ebae7f608cac701e4
Malcolm <cacheyourdreams@gmail.com>
2016-03-14 08:29:44 +0000

    allowing root login to facilitate database setup script

--- db.py
+++ db.py
@@ -2,8 +2,15 @@
 import MySQLdb
 
 class Database:
-	def __init__(self):
-		self.db = MySQLdb.connect(host="localhost", user="user4321", passwd="gryphon", db="comp4321")
+	def __init__ (self, username="user4321", password="gryphon", database="comp4321"):
+		if (database == None):
+			self.db = MySQLdb.connect(host="localhost", user=username, passwd=password)
+		else:
+			self.db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
 		self.cur = self.db.cursor() 
 	def query(self,query):
 		self.cur.execute(query)
+	def close(self):
+		self.db.commit()
+		self.cur.close()
+		self.db.close()
